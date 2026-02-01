4e513e306f49b9c5c26395a018004c661964bdeb
Romgi Productions <JonathanGraydon22@gmail.com>
2026-02-01 18:24:11 -0500

    Added speed insights from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "FRC 854 | Iron Bears",
@@ -17,6 +18,9 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+
+        {/* Vercel Speed Insights */}
+        <SpeedInsights />
       </body>
     </html>
   );
